9e73c9dd47823cd2388ed984fc72936b1a18f32f
viktor111 <swifrorlilko@gmail.com>
2023-04-08 09:55:52 +0300

    REFACTOR: Helpers

--- BoricaNet/Helpers/Generator.cs
+++ BoricaNet/Helpers/Generator.cs
@@ -1,3 +1,4 @@
+using BoricaNet.Exceptions;
 using System.Security.Cryptography;
 using System.Text;
 
@@ -5,41 +6,34 @@ namespace BoricaNet.Helpers;
 
 internal static class Generator
 {
-    public static string GenerateHexStringFromByteArray(byte[] byteArray)
+    public static string ByteArrayToHexString(byte[] byteArray)
     {
-        return BitConverter.ToString(byteArray).Replace("-","");
+        if (byteArray == null)
+            throw new BoricaNetException($"{nameof(byteArray)} is null");
+
+        return BitConverter.ToString(byteArray).Replace("-", "");
     }
-    
+
     public static byte[] GenerateRandomByteArray(int minLength, int maxLength)
     {
+        ValidateArrayLength(minLength, maxLength);
+
         var random = RandomNumberGenerator.Create();
-        var length = new byte[4];
-        random.GetBytes(length);
-        var arrayLength = BitConverter.ToInt32(length, 0);
-        arrayLength = Math.Abs(arrayLength % (maxLength - minLength + 1)) + minLength;
+        var arrayLength = GetRandomLength(random, minLength, maxLength);
+
         var array = new byte[arrayLength];
         random.GetBytes(array);
+
         return array;
     }
     
     public static int GenerateOrderNumber(string nonce)
     {
-        var data = BitConverter.GetBytes(DateTime.UtcNow.ToBinary());
-        var nonceBytes = Encoding.UTF8.GetBytes(nonce);
-        var inputData = new byte[data.Length + nonceBytes.Length];
+        var inputData = ConcatenateTimestampAndNonce(nonce);
 
-        Buffer.BlockCopy(data, 0, inputData, 0, data.Length);
-        Buffer.BlockCopy(nonceBytes, 0, inputData, data.Length, nonceBytes.Length);
-
-        byte[] hash;
-
-        using (var hmac = new HMACSHA256(GetKey()))
-        {
-            hash = hmac.ComputeHash(inputData);
-        }
+        var hash = ComputeHash(inputData);
 
         var randomNumber = BitConverter.ToInt32(hash, 0) % 900000 + 100000;
-
         
         var str = Math.Abs(randomNumber);
 
@@ -59,5 +53,42 @@ internal static class Generator
         rng.GetBytes(secretKey);
 
         return secretKey;
+    }
+
+    private static void ValidateArrayLength(int minLength, int maxLength)
+    {
+        if (minLength < 0)
+            throw new BoricaNetException($"{nameof(minLength)} must be non-negative.");
+
+        if (maxLength < minLength)
+            throw new ArgumentOutOfRangeException($"{nameof(maxLength)} must be greater than or equal to minLength.");
+    }
+
+    private static int GetRandomLength(RandomNumberGenerator random, int minLength, int maxLength)
+    {
+        var length = new byte[4];
+        random.GetBytes(length);
+        var arrayLength = BitConverter.ToInt32(length, 0);
+        arrayLength = Math.Abs(arrayLength % (maxLength - minLength + 1)) + minLength;
+
+        return arrayLength;
+    }
+
+    private static byte[] ConcatenateTimestampAndNonce(string nonce)
+    {
+        var data = BitConverter.GetBytes(DateTime.UtcNow.ToBinary());
+        var nonceBytes = Encoding.UTF8.GetBytes(nonce);
+        var inputData = new byte[data.Length + nonceBytes.Length];
+
+        Buffer.BlockCopy(data, 0, inputData, 0, data.Length);
+        Buffer.BlockCopy(nonceBytes, 0, inputData, data.Length, nonceBytes.Length);
+
+        return inputData;
+    }
+
+    private static byte[] ComputeHash(byte[] inputData)
+    {
+        using var hmac = new HMACSHA256(GetKey());
+        return hmac.ComputeHash(inputData);
     }
 }
